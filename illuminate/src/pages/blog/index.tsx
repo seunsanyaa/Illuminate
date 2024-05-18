eea28ac46d7dd9db20abc615c1e4982600ce3960
seunsanyaa <55216847+seunsanyaa@users.noreply.github.com>
2024-05-18 09:09:29 +0300

    changes

--- illuminate/src/pages/blog/index.tsx
+++ illuminate/src/pages/blog/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "~/components/header";
 
 export default function Blog() {
@@ -129,9 +130,12 @@ export default function Blog() {
             Learn how to grow your business with our expert advice.    </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
+            {posts.map((post) => (
+            
+              <Link   href={'/inner-blog'}            key={post.id}>
+                
             <article
-              key={post.id}
+
               className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
             >
               <img src={post.imageUrl} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
@@ -159,6 +163,8 @@ export default function Blog() {
                 </a>
               </h3>
             </article>
+
+                </Link>
           ))}
         </div>
       </div>
